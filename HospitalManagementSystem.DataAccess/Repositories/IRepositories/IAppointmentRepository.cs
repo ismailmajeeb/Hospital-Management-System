52f9f29f8f282d48afdf1856a8be7b9657113783
Ahmed Khaled Elsayed Ahmed <101284801+Ahmed-Khaled-Elsayed@users.noreply.github.com>
2024-10-06 18:02:28 +0300

    Create IAppointmentRepository

---
+++ HospitalManagementSystem.DataAccess/Repositories/IRepositories/IAppointmentRepository.cs
@@ -0,0 +1,14 @@
+using HospitalManagementSystem.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagementSystem.DataAccess.Repositories.IRepositories
+{
+    public interface IAppointmentRepository : IGenericRepository<Appointment>
+    {
+    }
+
+}
