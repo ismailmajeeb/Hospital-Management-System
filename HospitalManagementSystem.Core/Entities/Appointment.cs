fa2d05693b80e6fe88e1614524c5fc18adf1133c
Ahmed Khaled Elsayed Ahmed <101284801+Ahmed-Khaled-Elsayed@users.noreply.github.com>
2024-10-06 18:00:18 +0300

    Update Appointment Entity

--- HospitalManagementSystem.Core/Entities/Appointment.cs
+++ HospitalManagementSystem.Core/Entities/Appointment.cs
@@ -8,5 +8,14 @@ namespace HospitalManagementSystem.Core.Entities
 {
     public class Appointment
     {
+        public int AppointmentID { get; set; }
+        public int PatientID { get; set; }
+        public int DoctorID { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public TimeSpan AppointmentTime { get; set; }
+        public string Reason { get; set; }
+
+        public Patient Patient { get; set; }
+        public Doctor Doctor { get; set; }
     }
 }
