7d440b5ce69027b53d1f5026159570a90e1f4059
Ahmed Khaled Elsayed Ahmed <101284801+Ahmed-Khaled-Elsayed@users.noreply.github.com>
2024-10-06 18:06:05 +0300

    Create AppointmentConfiguration

---
+++ HospitalManagementSystem.DataAccess/Persistence/Configrautions/AppointmentConfiguration.cs
@@ -0,0 +1,34 @@
+using HospitalManagementSystem.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagementSystem.DataAccess.Persistence.Configrautions
+{
+    internal class AppointmentConfiguration : IEntityTypeConfiguration<Appointment>
+    {
+        public void Configure(EntityTypeBuilder<Appointment> builder)
+        {
+            builder.HasKey(a => a.AppointmentID);
+            builder.Property(a => a.AppointmentID).ValueGeneratedOnAdd();
+
+            builder.Property(a => a.PatientID).IsRequired();
+            builder.Property(a => a.DoctorID).IsRequired();
+            builder.Property(a => a.AppointmentDate).IsRequired();
+            builder.Property(a => a.AppointmentTime).IsRequired();
+            builder.Property(a => a.Reason).HasMaxLength(500);
+
+            builder.HasOne(a => a.Patient)
+                   .WithMany(p => p.Appointments)
+                   .HasForeignKey(a => a.PatientID);
+
+            builder.HasOne(a => a.Doctor)
+                   .WithMany(d => d.Appointments)
+                   .HasForeignKey(a => a.DoctorID);
+        }
+    }
+}
