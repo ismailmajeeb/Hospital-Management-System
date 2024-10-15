930668a553e4acdffd96fbffaef7d609809f2e35
Ahmed Khaled Talha <101284801+Ahmed-Khaled-Elsayed@users.noreply.github.com>
2024-10-15 21:14:36 +0300

    Add files via upload

---
+++ HospitalManagementSystem.Application/Models/Doctors/DoctorDashBoardModel.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+using HospitalManagementSystem.Core.Entities;
+
+namespace HospitalManagementSystem.Application.Models.Doctors
+{
+    public class DoctorDashBoardModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Gender Gender { get; set; }
+        public ICollection<Patient>? Patients { get; set; }
+        public ICollection<Appointment>? Appointments { get; set; }
+    }
+}
