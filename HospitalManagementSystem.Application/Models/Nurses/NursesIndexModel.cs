d4695b42bccdb64ad52f3fffc3063f9413ede828
Ahmed Khaled Talha <101284801+Ahmed-Khaled-Elsayed@users.noreply.github.com>
2024-10-15 09:33:37 +0300

    Add files via upload

---
+++ HospitalManagementSystem.Application/Models/Nurses/NursesIndexModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagementSystem.Application.Models.Nurses
+{
+    public class NursesIndexModel
+    {
+        public string UserId { get; set; }
+        public string Name { get; set; }
+    }
+}
